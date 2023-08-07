65a4f3cf18338d8e57e871f9eb42056ada034b13
Enzo <enzo.rva@gmail.com>
2023-08-07 18:56:33 -0300

    Geolocator code

---
+++ lib/services/location.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/widgets.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  late double latitude;
+  late double longitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      final permission = await Geolocator.requestPermission();
+      //final permission = await _geolocatorPlatform.checkPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
